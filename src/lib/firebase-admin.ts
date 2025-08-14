83f169b02f60611b697b5275e9dbcd926b053ff1
Saheed Adewumi <adewumiadewale493@gmail.com>
2025-08-14 14:10:23 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -3,9 +3,9 @@ import { getFirestore } from 'firebase-admin/firestore';
 import { ServiceAccount } from 'firebase-admin';
 
 const serviceAccount: ServiceAccount = {
-    project_id: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
-    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    project_id: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || '',
+    private_key: (process.env.FIREBASE_PRIVATE_KEY || '').replace(/\\n/g, '\n'),
+    client_email: process.env.FIREBASE_CLIENT_EMAIL || '',
 };
 
 const app = !getApps().length ? initializeApp({ credential: cert(serviceAccount) }) : getApp();
